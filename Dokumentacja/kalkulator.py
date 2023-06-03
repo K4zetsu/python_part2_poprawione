4d3260be514a6fd92a93ebe574bfd719d71e1e7e
k4zetsu <trzeciak.piotr26@gmail.com>
2023-06-03 18:31:20 +0200

    Poprawione zadania czesc 2

--- Dokumentacja/kalkulator.py
+++ Dokumentacja/kalkulator.py
@@ -12,17 +12,25 @@ while True:
 #warunek 'if' sprawdzający, którą opcję wybrał użytkownik
     if wybor == 1:
 #tutaj jak i w pozostałych opcjach, wywołanie odpowiedniej funkcji z zaimportowanego pliku
-        mod1.dodawanie()
+        a = int(input("Podaj pierwszą liczbę: "))
+        b = int(input("Podaj drugą liczbę: "))
+        print(f"Wynik dodawania {a} + {b} to : {mod1.dodawanie(a, b)}")
     elif wybor == 2:
-        mod1.odejmowanie()
+        a = int(input("Podaj pierwszą liczbę: "))
+        b = int(input("Podaj drugą liczbę: "))
+        print(f"Wynik odejmowania {a} - {b} to : {mod1.odejmowanie(a, b)}")
     elif wybor == 3:
-        mod1.mnozenie()
+        a = int(input("Podaj pierwszą liczbę: "))
+        b = int(input("Podaj drugą liczbę: "))
+        print(f"Wynik mnożenia {a} * {b} to : {mod1.mnozenie(a, b)}")
     elif wybor == 4:
 # 'try' sprawdzający walidację danych przy wykonywaniu dzielenia - jeśli użytkownik wpisze '0' jako dzielnik
 # program zgłosi ZeroDivisionError, który zostanie obsłużony poleceniami w segmencie 'except' czyli wyświetli stosowny komunikat i przejdzie
 # do następnej iteracji pętli
         try:
-            mod1.dzielenie()
+            a = int(input("Podaj pierwszą liczbę: "))
+            b = int(input("Podaj drugą liczbę: "))
+            print(f"Wynik dzielenia {a} / {b} to : {mod1.dzielenie(a, b)}")
         except ZeroDivisionError:
             print("Nie możesz dzielić przez 0...")
             continue
